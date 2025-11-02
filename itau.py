a1790a295fac2ce517b353075818e5ba6b5594df
abreuzzzz <105026818+abreuzzzz@users.noreply.github.com>
2025-11-02 15:12:43 -0300

    Atualizar o itau.py

--- itau.py
+++ itau.py
@@ -1,6 +1,10 @@
 import requests
 import os
 import json
+import urllib3
+
+# Desabilita warning de requisição HTTPS sem verificação de certificado
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 API_URL = 'https://api.itauunibancoclube.com.br/notification-service/api/v1/beneficiary/banners?pageId=1'
 CACHE_FILE = 'last_banners.json'
@@ -20,23 +24,23 @@ def save_current(data):
     with open(CACHE_FILE, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-def notify_telegram(content):
+def notify_telegram():
     token = os.getenv("TELEGRAM_BOT_TOKEN2")
     chat_id = os.getenv("TELEGRAM_CHAT_ID2")
     if not (token and chat_id):
         print("Configuração do Telegram ausente")
         return
     url = f'https://api.telegram.org/bot{token}/sendMessage'
-    msg = "👍 Atualização detectada nos banners: \n\n" + json.dumps(content, ensure_ascii=False, indent=2)
+    msg = "👍 Atualização detectada nos banners do Itaú."
     data = {"chat_id": chat_id, "text": msg}
     resp = requests.post(url, json=data)
-    print("Telegram status:", resp.status_code)
+    print("Telegram status:", resp.status_code, resp.text)
 
 if __name__ == "__main__":
     current = fetch_data()
     last = load_last()
     if current != last:
-        notify_telegram(current)
+        notify_telegram()
         save_current(current)
     else:
         print("Nenhuma alteração detectada")
